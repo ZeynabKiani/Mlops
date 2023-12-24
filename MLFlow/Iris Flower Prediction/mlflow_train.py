56bc9b749c599b5e8220b950489db13ef13ee5d8
ZeynabKiani <59164384+ZeynabKiani@users.noreply.github.com>
2023-12-24 18:41:17 +0330

    Organized code structure for better modularity

--- MLFlow/Iris Flower Prediction/mlflow_train.py
+++ MLFlow/Iris Flower Prediction/mlflow_train.py
@@ -7,6 +7,11 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 import os
 
+def load_and_split_data():
+    # Load Iris dataset and split into training and testing sets
+    data = load_iris()
+    return train_test_split(data.data, data.target, test_size=0.2, random_state=42)
+
 def train_random_forest(X_train, y_train, n_estimators=100, random_state=42):
     # Train RandomForestClassifier
     rf_model = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state)
@@ -14,29 +19,18 @@ def train_random_forest(X_train, y_train, n_estimators=100, random_state=42):
     return rf_model
 
 def evaluate_model(model, X_test, y_test):
-    # Make predictions on the test set
+    # Make predictions on the test set and calculate metrics
     y_pred = model.predict(X_test)
-
-    # Calculate metrics
     accuracy = accuracy_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred, average='weighted')
     recall = recall_score(y_test, y_pred, average='weighted')
-    
     return accuracy, precision, recall
 
-def log_to_mlflow(model, accuracy, precision, recall, params, model_name="random_forest_model"):
+def log_to_mlflow(model, params, metrics, model_name="random_forest_model"):
     # Log parameters and metrics to MLflow
     with mlflow.start_run():
-        # Log model parameters
-        for key, value in params.items():
-            mlflow.log_param(key, value)
-
-        # Log metrics
-        mlflow.log_metric("accuracy", accuracy)
-        mlflow.log_metric("precision", precision)
-        mlflow.log_metric("recall", recall)
-
-        # Log the model
+        mlflow.log_params(params)
+        mlflow.log_metrics(metrics)
         mlflow.sklearn.log_model(model, model_name)
 
 def save_test_data(X_test, feature_names, output_path="data/test_data.csv"):
@@ -45,9 +39,8 @@ def save_test_data(X_test, feature_names, output_path="data/test_data.csv"):
     pd.DataFrame(X_test, columns=feature_names).to_csv(output_path, index=False)
 
 if __name__ == "__main__":
-    # Load Iris dataset
-    data = load_iris()
-    X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
+    # Load and split data
+    X_train, X_test, y_train, y_test = load_and_split_data()
 
     # Train RandomForestClassifier
     rf_model = train_random_forest(X_train, y_train, n_estimators=100, random_state=42)
@@ -57,7 +50,8 @@ if __name__ == "__main__":
 
     # Log to MLflow
     params = {"n_estimators": 100, "random_state": 42}
-    log_to_mlflow(rf_model, accuracy, precision, recall, params)
+    metrics = {"accuracy": accuracy, "precision": precision, "recall": recall}
+    log_to_mlflow(rf_model, params, metrics)
 
     # Save the test data
-    save_test_data(X_test, data.feature_names)
+    save_test_data(X_test, load_iris().feature_names)
