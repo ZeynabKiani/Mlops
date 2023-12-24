9e05900a972c80eb51ea506e66798a235c84decd
ZeynabKiani <59164384+ZeynabKiani@users.noreply.github.com>
2023-12-24 18:46:45 +0330

    Maintained clear separation of concerns for different functionalities

--- MLFlow/Iris Flower Prediction/mlflow_train.py
+++ MLFlow/Iris Flower Prediction/mlflow_train.py
@@ -7,10 +7,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 import os
 
-def load_and_split_data():
+def load_and_split_data(test_size=0.2, random_state=42):
     # Load Iris dataset and split into training and testing sets
     data = load_iris()
-    return train_test_split(data.data, data.target, test_size=0.2, random_state=42)
+    return train_test_split(data.data, data.target, test_size=test_size, random_state=random_state)
 
 def train_random_forest(X_train, y_train, n_estimators=100, random_state=42):
     # Train RandomForestClassifier
