c911d18b6a88fabd9d6afb8259b68c10151e8100
ZeynabKiani <59164384+ZeynabKiani@users.noreply.github.com>
2023-12-29 18:41:30 +0330

    Consolidated the app creation and execution logic

--- MLFlow/Iris Flower Prediction/mlflow_server.py
+++ MLFlow/Iris Flower Prediction/mlflow_server.py
@@ -9,13 +9,12 @@ class ModelServer:
     def predict(self, input_data):
         try:
             predictions = self.loaded_model.predict(input_data)
-            
             return predictions.tolist()
 
         except Exception as e:
             raise ValueError(f"Prediction error: {str(e)}")
 
-def create_app(model_path="random_forest_model"):
+def create_and_run_app(model_path="random_forest_model"):
     app = Flask(__name__)
     model_server = ModelServer(model_path)
 
@@ -31,8 +30,11 @@ def create_app(model_path="random_forest_model"):
         except ValueError as ve:
             return jsonify({'error': str(ve)})
 
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000)
+
     return app
 
+# Load the saved model server and run the Flask app
 if __name__ == '__main__':
-    app = create_app()
-    app.run(host='0.0.0.0', port=5000)
+    create_and_run_app()
