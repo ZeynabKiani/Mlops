d15cac368e11f04e6c75ed275663ce0ca5de8bc0
ZeynabKiani <59164384+ZeynabKiani@users.noreply.github.com>
2024-01-10 23:54:12 +0330

    Implement weighted precision and recall metrics

--- MLFlow/Iris Flower Prediction/mlflow_train.py
+++ MLFlow/Iris Flower Prediction/mlflow_train.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 import os
 
