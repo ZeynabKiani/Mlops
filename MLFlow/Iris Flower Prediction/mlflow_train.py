4e6e873f5d63332a8b0baf55ca20c89dcf3e6801
ZeynabKiani <59164384+ZeynabKiani@users.noreply.github.com>
2024-01-10 23:54:28 +0330

    Refactor MLFlow logging for better organization

--- MLFlow/Iris Flower Prediction/mlflow_train.py
+++ MLFlow/Iris Flower Prediction/mlflow_train.py
@@ -4,7 +4,6 @@ import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 import os
 
